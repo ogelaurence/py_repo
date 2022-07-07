0c020a3ffe47c4a4d2b8bc7d579452e282e7c655
ogelaurence <ogelaurence@gmail.com>
2022-07-07 16:57:20 +0100

    All files commited

--- calc.py
+++ calc.py
@@ -1,1 +1,17 @@
-addition Substraction Division Multiplication
+import streamlit as st
+
+st.sidebar.header(" This is a simple calculator ")
+x = st.number_input( " Choose first no : ")
+y = st.number_input(" Choose second no : ")
+
+col1,col2 = st.columns(2)
+with col1 :
+    if st.button(" Add"):
+        z = x + y
+        st.write( f''' Answer is \n
+                    {z} ''')
+with col2:
+    if st.button(" Sub "):
+        z = x- y
+        st.write(f''' Answer is \n
+                    {z} ''')
